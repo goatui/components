56b1c641a69b743580e6493c787d240c2de88ffa
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2023-10-14 23:11:47 +0530

    icons documentation fix

--- icons-update-from-metadata.ts
+++ icons-update-from-metadata.ts
@@ -1,18 +1,37 @@
 // Run in deno: deno run -A icons-update-from-metadata.ts
-import {ICON_BASE_URL} from "./src/components/general/icon/constants.ts";
+import { ICON_BASE_URL } from './src/components/general/icon/constants.ts';
 
 const response = await fetch(ICON_BASE_URL + '/metadata.json');
 
 const result = await response.json();
 
-const outputJson = result.icons.map((icon)=>{
-  return {
-    name: icon.name,
-    label: icon.friendlyName,
-    path: icon.assets[icon.assets.length - 1].filepath
-  }
-});
+await Deno.writeTextFile(
+  './src/components/general/icon/icons.ts',
+  `export default ${JSON.stringify(
+    result.icons.map(icon => {
+      return {
+        name: icon.name,
+        label: icon.friendlyName,
+        path: icon.assets[icon.assets.length - 1].filepath,
+      };
+    }),
+    null,
+    2,
+  )}`,
+);
 
-await Deno.writeTextFile('./src/components/general/icon/icons.ts', `export default ${JSON.stringify(outputJson, null, 2)}`);
-
-
+await Deno.writeTextFile(
+  './docs/assets/icons.json',
+  `${JSON.stringify(
+    result.icons.map(icon => {
+      return {
+        name: icon.name,
+        label: icon.friendlyName,
+        path: icon.assets[icon.assets.length - 1].filepath,
+        aliases: icon.aliases,
+      };
+    }),
+    null,
+    2,
+  )}`,
+);
