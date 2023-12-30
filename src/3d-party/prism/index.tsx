875a76d005ef1db89564430fc2bad25ccdd483f1
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2023-12-30 17:41:21 +0530

    check in prismjs

--- src/3d-party/prism/index.tsx
+++ src/3d-party/prism/index.tsx
@@ -1,9 +1,10 @@
-import {loadScript} from '../../utils/utils';
+import { loadScript } from '../../utils/utils';
+import { getAssetPath } from '@stencil/core';
 
-const VERSION = '1.27.0';
+//const VERSION = '1.27.0';
 
 export async function loadPrism() {
   // @ts-ignore
-  await loadScript(`https://cdn.jsdelivr.net/npm/prismjs@${VERSION}/components/prism-core.min.js`);
-  await loadScript(`https://cdn.jsdelivr.net/npm/prismjs@${VERSION}/plugins/autoloader/prism-autoloader.min.js`);
+  await loadScript(getAssetPath('./assets/node_modules/prismjs/components/prism-core.min.js'));
+  await loadScript(getAssetPath('./assets/node_modules/prismjs/plugins/autoloader/prism-autoloader.min.js'));
 }
