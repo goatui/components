d2fb9128e7c63428f426816695693283ec0006ee
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-01-08 16:53:38 +0530

    restore blog posts

--- astro-docs/astro.config.mjs
+++ astro-docs/astro.config.mjs
@@ -1,4 +1,11 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({site: 'https://goatui.com', compressHTML: false, server: {port: 4000, host: true}});
+export default defineConfig({
+    site: 'https://goatui.com',
+    compressHTML: false,
+    server: { port: 4000, host: true },
+    integrations: [sitemap()],
+  },
+);
