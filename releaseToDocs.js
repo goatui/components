abe31dabc9c3d6b6e057c22fd5078ea00a417462
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-05-14 17:06:14 +0530

    feat:  add fluid text styles + remove jekyll old docs.

--- releaseToDocs.js
+++ releaseToDocs.js
@@ -4,16 +4,6 @@ function releaseToDocs(cb) {
   const packageJsonStr = fs.readFileSync('package.json');
   const packageJson = JSON.parse(packageJsonStr);
 
-  fs.readFile('docs/_config.yml', 'utf8', function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
-    let result = data.replace(new RegExp(/@goatui\/components@[0-9]+[.][0-9]+[.][0-9]+\/dist/, 'g'), `@goatui/components@${packageJson.version}/dist`);
-    fs.writeFile('docs/_config.yml', result, 'utf8', function (err) {
-      if (err) return console.log(err);
-      cb('Docs config updated');
-    });
-  });
   fs.readFile('astro-docs/src/_data/site.json', 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
