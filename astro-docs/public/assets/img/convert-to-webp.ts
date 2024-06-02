3f9dec2ce1590437d94a988c9f4c1eb06b5b2b5b
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-06-02 07:38:59 +0530

    docs: uptake new astro

--- astro-docs/public/assets/img/convert-to-webp.ts
+++ astro-docs/public/assets/img/convert-to-webp.ts
@@ -1,19 +1,19 @@
 import * as sharp from 'npm:sharp@0.33.4';
-import { walkSync } from "https://deno.land/std@0.193.0/fs/mod.ts";
+import { walkSync } from 'https://deno.land/std@0.193.0/fs/mod.ts';
 
-for (const file of walkSync('.',{
-    maxDepth: 1,
-    includeDirs: false,
-    exts: [".png"]
-  })) {
-    console.log(file);
-    sharp
-  .default(file.name)
-  .toFile(file.name.replace(".png", '.webp') , (err, info) => {
+for (const file of walkSync('.', {
+  maxDepth: 1,
+  includeDirs: false,
+  exts: ['.png'],
+})) {
+  console.log(file);
+  sharp.default(file.name).toFile(file.name.replace('.png', '.webp'), (err, info) => {
     console.log(err, info);
   });
 }
 
-/*
- * deno run --allow-read --allow-write --allow-ffi --allow-env test.ts
+/**
+ * ```cmd
+ * deno run -A public/assets/img/convert-to-webp.ts
+ * ```
  */
