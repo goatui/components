444b6bb008dad200096350577f565905c677dc8b
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-06-14 21:45:55 +0530

    fix: notification style issue

--- astro-docs/bin/convert-to-webp.ts
+++ astro-docs/bin/convert-to-webp.ts
@@ -1,7 +1,7 @@
 import * as sharp from 'npm:sharp@0.33.4';
 import { walkSync } from 'https://deno.land/std@0.193.0/fs/mod.ts';
 
-for (const file of walkSync('.', {
+for (const file of walkSync('../public/assets/img', {
   maxDepth: 1,
   includeDirs: false,
   exts: ['.png'],
